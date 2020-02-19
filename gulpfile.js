2f5c889321c4dcbd60c8a3e9a072271cac45a16a
D1VORY <dubchakob@gmail.com>
2020-02-19 18:04:10 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+// Gulp.js configuration
+
+const
+  // modules
+  gulp = require('gulp'),
+
+  // development mode?
+  devBuild = (process.env.NODE_ENV !== 'production'),
+
+  // folders
+  src = 'src/',
+  build = 'build/'
