300454a2006cf6f5fb5968aa41a75cf81e90bab7
D1VORY <dubchakob@gmail.com>
2020-02-20 21:05:14 +0200

    added header and section 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,45 @@
 // Gulp.js configuration
 
-const
-  // modules
-  gulp = require('gulp'),
+// const
+//   // modules
+//   gulp = require('gulp'),
 
-  // development mode?
-  devBuild = (process.env.NODE_ENV !== 'production'),
+//   // development mode?
+//   devBuild = (process.env.NODE_ENV !== 'production'),
 
-  // folders
-  src = 'src/',
-  build = 'build/'
+//   // folders
+//   src = 'src/',
+//   build = 'build/'
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+.pipe(sass())
+.pipe(gulp.dest("src/css"))
+.pipe(browserSync.stream());
+});
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+.pipe(gulp.dest("src/js"))
+.pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series('sass', function() {
+
+browserSync.init({
+server: "./src"
+});
+
+gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], gulp.series('sass'));
+gulp.watch("src/html/*.html").on('change', browserSync.reload);
+
+}));
+
+gulp.task("default", gulp.series("js", "serve"));
